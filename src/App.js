38217132a87e685200ccdff4fe118a643420df7b
Lucas Hill <lucashill1717@gmail.com>
2023-11-30 20:14:22 -0700

    colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 
 export default function App() {
   return (
-    <div>
+    <>
+      <div className='navbar'>
+
+      </div>
       <h1>Lucas Hill</h1>
       <p>words words words im using react</p>
-    </div>
+    </>
   );
 }
