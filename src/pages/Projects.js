8823c1b65ba94413c88091db15a3d327361e168f
Lucas Hill <lucashill1717@gmail.com>
2023-12-05 14:40:42 -0700

    infinitely responsive

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,6 +4,10 @@ export default function Projects() {
     return (
       <>
         <NavBar/>
+        <h1 id='sorry'>
+          Sorry, nothing ready to be hosted here quite yet.<br/>
+          Stay tuned.
+        </h1>
       </>
     );
 }
