cbb8b2b8979b863d342458b5383f6a2027761896
Lucas Hill <lucashill1717@gmail.com>
2023-12-08 01:17:52 -0700

    about progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,15 @@ export default function About() {
   return (
     <div className='about-background'>
       <NavBar/>
+      <h1 id='about-me'>About Me</h1>
+      <div className='about-box'>
+        <p>
+          Hello, my name is Lucas Hill. I am an aspiring junior
+          developer that wants to leave an impact. This website is
+          my first large personal project, and I have plans for more
+          to come, hosted here.
+        </p>
+      </div>
     </div>
   );
 }
